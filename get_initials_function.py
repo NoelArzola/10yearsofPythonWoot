803b7baf1f47328dd0c8ff056505e09dcb835468
Noel Arzola <noel.arzola@gmail.com>
2021-04-06 13:30:51 -0400

    Cleanup, added on, functions continued

--- get_initials_function.py
+++ get_initials_function.py
@@ -1,6 +1,9 @@
 # This function will return the first initial of a name
-def get_initial(name):
-    initial = name[0:1].upper
+def get_initial(name, force_uppercase=True):
+    if force_uppercase:
+        initial = name[0:1].upper()
+    else:
+        initial = name[0:1]
     return initial
 
 # Ask someones name and return the initials
@@ -8,9 +11,9 @@ first_name = input('What is your first name? ')
 first_name_initial = get_initial(first_name)
 
 middle_name = input('What is your middle name? ')
-middle_name_initial = get_initial(middle_name)
+middle_name_initial = get_initial(middle_name, False)
  
 last_name = input('What is your last name? ')
-last_name_initial = get_initial(last_name)
+last_name_initial = get_initial(last_name, False)
 
 print('Your initials are: ' + first_name_initial + middle_name_initial + last_name_initial)
