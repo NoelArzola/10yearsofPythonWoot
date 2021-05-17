d639c9f245e29710e7c65c94f31b680e47831191
Noel Arzola Jr <noel.arzola@gmail.com>
2021-05-17 15:01:11 -0400

    More on inheritance

--- 2 - more/inheritance_demo.py
+++ 2 - more/inheritance_demo.py
@@ -10,11 +10,19 @@ class Student(Person):
 		self.school = school
 	def sing_school_song(self):
 		print('Ode to ' + self.school)
+	def say_hello(self):
+			# Let the parent do some work
+			super().say_hello()
+			# Add on custom code
+			print('I am feeling good')
+	def __str__(self):
+			return f'{self.name} attends {self.school}'
 
 student = Student('Noel', 'Fredonia')
 student.say_hello()
 student.sing_school_song()
 # What are you?
-print(isinstance(student, Student))
-print(isinstance(student, Person))
-print(issubclass(Student, Person))
+print(f'Is this a student? {isinstance(student, Student)}')
+print(f'Is this a person? {isinstance(student, Person)}')
+print(f'Is Student a Person? {issubclass(Student, Person)}')
+print(student)
